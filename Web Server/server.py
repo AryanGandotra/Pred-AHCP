0c19c7581d15e1c2f58598ec9aa5d6b98d45de92
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2024-08-13 21:20:32 +0530

    Update server.py

--- Web Server/server.py
+++ Web Server/server.py
@@ -573,13 +573,8 @@ def process():
     for i in data:
         result = model.predict_proba([i])
         probability = result[0]
-
-        if probability[0] > probability[1]:
-            all_probabilities.append(probability[0])
-            all_labels.append("Negative")  # TO BE CONFIRMED
-        else:
-            all_probabilities.append(probability[1])
-            all_labels.append("Positive")  # TO BE CONFIRMED
+        all_probabilities.append(probability[1])
+        all_labels.append("AHCP") 
         all_labels.append(model.predict([i])[0])
 
     return render_template(
